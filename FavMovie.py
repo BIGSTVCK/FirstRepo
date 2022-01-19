182713d3a1fd923e0d6dab5c18410f1d685ea60c
Stvckey <97483875+BIGSTVCK@users.noreply.github.com>
2022-01-19 18:26:35 -0500

    Update FavMovie.py
    Added a final parenthesis to the end of the file.

--- FavMovie.py
+++ FavMovie.py
@@ -1,1 +1,1 @@
-print("Hello world! My favorite movie is the original Spider-Man. Y'know, the one where Green Goblin dominaqtes Peter."
+print("Hello world! My favorite movie is the original Spider-Man. Y'know, the one where Green Goblin dominaqtes Peter.")
